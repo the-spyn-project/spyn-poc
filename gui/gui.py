ecb653fae8a234fcc7c039d9c9cdeb2a258b3d59
Anish Agarwal <a22agarw@outlook.com>
2018-05-12 18:24:50 -0700

    Added provider, commander and account tabs to GUI

--- gui/gui.py
+++ gui/gui.py
@@ -31,19 +31,28 @@ class spyn:
     def __init__(self):
 
         self.initialize_gui()
-        self.initialize_seller_frame()
+        self.initialize_tab_buttons()
+        self.initialize_provider_frame()
+        self.initialize_commander_frame()
+        self.initialize_account_frame()
 
     def initialize_gui(self):
 
-        self.gui_w = 900
-        self.gui_h = 600
+        self.gui_w = 1500
+        self.gui_h = 800
 
-        self.seller_frame_h = 200
-        self.seller_frame_w = 300
-        self.seller_frame_x = 3
-        self.seller_frame_y = 3
+        self.tab_btn_h = 25
+        self.tab_btn_w = 150
+        self.tab_btn_x = 20
+        self.tab_btn_y = 20
 
-        self.sample_btn_h = 25
+        self.tab_container_h = self.gui_h - (self.tab_btn_y + self.tab_btn_h) - self.tab_btn_x
+        self.tab_container_w = 700
+        self.tab_container_x = self.tab_btn_x + 1  # 1 offset is required to be aligned
+        self.tab_container_y = self.tab_btn_y + self.tab_btn_h
+        self.container_offset = 2
+
+        self.sample_btn_h = 30
         self.sample_btn_w = 150
         self.sample_btn_x = 5
         self.sample_btn_y = 5
@@ -54,28 +63,136 @@ class spyn:
         self.gui.title('SPYN')
         self.gui.iconbitmap(r'C:\Users\Veda Sadhak\Desktop\GIT\spyn-poc\images\logo.ico')
 
-    def initialize_seller_frame(self):
+    def initialize_tab_buttons(self):
 
-        seller_frame_container = tk.Frame(self.gui)
-        seller_frame_container.place(height=self.seller_frame_h+2,
-                           width=self.seller_frame_w+2,
-                           x=self.seller_frame_x,
-                           y=self.seller_frame_y)
+        provider_btn = tk.Button(self.gui,
+                                 text='Provider',
+                                 font='Arial 8',
+                                 command=lambda: self.toggle_tabs('provider'))
+        provider_btn.place(height=self.tab_btn_h,
+                           width=self.tab_btn_w,
+                           x=self.tab_btn_x,
+                           y=self.tab_btn_y)
 
-        seller_frame = tk.LabelFrame(seller_frame_container,text='Seller')
-        seller_frame.place(height=self.seller_frame_h,
-                           width=self.seller_frame_w,
-                           x=self.seller_frame_x,
-                           y=self.seller_frame_y)
+        commander_btn = tk.Button(self.gui,
+                                 text='Commander',
+                                 font='Arial 8',
+                                 command=lambda: self.toggle_tabs('commander'))
+        commander_btn.place(height=self.tab_btn_h,
+                            width=self.tab_btn_w,
+                            x=self.tab_btn_x+self.tab_btn_w,
+                            y=self.tab_btn_y)
 
-        sample_btn = tk.Button(seller_frame,
-                               text='Sample Button',
+        account_btn = tk.Button(self.gui,
+                                 text='Account',
+                                 font='Arial 8',
+                                 command=lambda: self.toggle_tabs('account'))
+        account_btn.place(height=self.tab_btn_h,
+                          width=self.tab_btn_w,
+                          x=self.tab_btn_x+self.tab_btn_w*2,
+                          y=self.tab_btn_y)
+
+    def initialize_provider_frame(self):
+
+        self.provider_frame_container = tk.Frame(self.gui)
+        self.provider_frame_container.place(height=self.tab_container_h+self.container_offset,
+                                            width=self.tab_container_w+self.container_offset,
+                                            x=self.tab_container_x,
+                                            y=self.tab_container_y)
+
+        provider_frame = tk.LabelFrame(self.provider_frame_container,text='')
+        provider_frame.place(height=self.tab_container_h,
+                             width=self.tab_container_w,
+                             x=0,
+                             y=0)
+
+        sample_btn = tk.Button(provider_frame,
+                               text='Sample Button 1',
                                font='Arial 8',
                                command=lambda:self.sample_function())
+
         sample_btn.place(height=self.sample_btn_h,
                          width=self.sample_btn_w,
                          x=self.sample_btn_x,
                          y=self.sample_btn_y)
+
+    def initialize_commander_frame(self):
+
+        self.commander_frame_container = tk.Frame(self.gui)
+        self.commander_frame_container.place(height=self.tab_container_h+self.container_offset,
+                                             width=self.tab_container_w+self.container_offset,
+                                             x=self.tab_container_x,
+                                             y=self.tab_container_y)
+
+        commander_frame = tk.LabelFrame(self.commander_frame_container,text='')
+        commander_frame.place(height=self.tab_container_h,
+                              width=self.tab_container_w,
+                              x=0,
+                              y=0)
+
+        sample_btn = tk.Button(commander_frame,
+                               text='Sample Button 2',
+                               font='Arial 8',
+                               command=lambda:self.sample_function())
+
+        sample_btn.place(height=self.sample_btn_h,
+                         width=self.sample_btn_w,
+                         x=self.sample_btn_x,
+                         y=self.sample_btn_y)
+
+    def initialize_account_frame(self):
+
+        self.account_frame_container = tk.Frame(self.gui)
+        self.account_frame_container.place(
+            height=self.tab_container_h + self.container_offset,
+            width=self.tab_container_w + self.container_offset,
+            x=self.tab_container_x,
+            y=self.tab_container_y)
+
+        account_frame = tk.LabelFrame(self.account_frame_container, text='')
+        account_frame.place(height=self.tab_container_h,
+                              width=self.tab_container_w,
+                              x=0,
+                              y=0)
+
+        sample_btn = tk.Button(account_frame,
+                               text='Sample Button 3',
+                               font='Arial 8',
+                               command=lambda: self.sample_function())
+
+        sample_btn.place(height=self.sample_btn_h,
+                         width=self.sample_btn_w,
+                         x=self.sample_btn_x,
+                         y=self.sample_btn_y)
+
+    def toggle_tabs(self,tab):
+
+        if tab == 'provider':
+            self.commander_frame_container.place_forget()
+            self.account_frame_container.place_forget()
+            self.provider_frame_container.place(height=self.tab_container_h+self.container_offset,
+                                                width=self.tab_container_w+self.container_offset,
+                                                x=self.tab_container_x,
+                                                y=self.tab_container_y)
+            logger.info("Provider frame is now viewable")
+
+        elif tab == 'commander':
+            self.provider_frame_container.place_forget()
+            self.account_frame_container.place_forget()
+            self.commander_frame_container.place(height=self.tab_container_h+self.container_offset,
+                                                 width=self.tab_container_w+self.container_offset,
+                                                 x=self.tab_container_x,
+                                                 y=self.tab_container_y)
+            logger.info("Commander frame is now viewable")
+
+        elif tab == 'account':
+            self.provider_frame_container.place_forget()
+            self.commander_frame_container.place_forget()
+            self.account_frame_container.place(height=self.tab_container_h + self.container_offset,
+                                               width=self.tab_container_w + self.container_offset,
+                                               x=self.tab_container_x,
+                                               y=self.tab_container_y)
+            logger.info("Account frame is now viewable")
 
     def sample_function(self):
 
