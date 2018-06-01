0aa8e59bb2a00ce7527098e75fed41893caa1bfc
zhxl0903 <zhxl0903@gmail.com>
2018-06-01 19:43:52 -0400

    Tested page loading via button click.

--- SpynGUI/renderer.js
+++ SpynGUI/renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { remote } = require('electron')
+const url = require('url')
+const path = require('path')
+
+function openMain() {
+   remote.getCurrentWindow().loadURL(url.format({
+    pathname: path.join(__dirname, 'spyn_main.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+document.querySelector('#submit').addEventListener('click', openMain)
