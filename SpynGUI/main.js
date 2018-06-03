041ca37a8cc7cb953f1afbef88d11700e1e383a3
veda-s4dhak <a22agarw@outlook.com>
2018-06-03 11:09:59 -0700

    Changed variable names, restructured code, added comments

--- SpynGUI/main.js
+++ SpynGUI/main.js
@@ -4,40 +4,40 @@ const path = require('path');
 const url = require('url');
 const electron = require('electron');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow} = electron;  //Loading specfic modules from Electron
 
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let main_window  // Keep a global reference of the window object
 
-function createWindow () {
+
+// Functions
+// =================================================================================================
+
+function start_main_window ()
+{
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 500})
+  main_window = new BrowserWindow({width: 800, height: 500})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
+  //Load the index.html of the app.
+  main_window.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  main_window.on('closed', function () {
     // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+    // in an array if your app supports multi windows.
+    main_window = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+
+// Main
+// =================================================================================================
+
+app.on('ready', start_main_window)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -52,9 +52,6 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    start_main_window()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
